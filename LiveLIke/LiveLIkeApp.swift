505f044e7fded72cd0b55235fddc0ec19abef81a
Javier Etxarri <javier.etxarri@openbravo.com>
2023-08-11 06:34:45 +0200

    Initial Commit

---
+++ LiveLIke/LiveLIkeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveLIkeApp.swift
+//  LiveLIke
+//
+//  Created by Javier Etxarri on 11/8/23.
+//
+
+import SwiftUI
+
+@main
+struct LiveLIkeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
