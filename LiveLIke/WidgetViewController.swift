92ae207cc81bcca3e20743be9f42ead6651eb840
Javier Etxarri <javier.etxarri@openbravo.com>
2023-08-28 08:56:02 +0200

    Adding delegate to the widget

--- LiveLIke/WidgetViewController.swift
+++ LiveLIke/WidgetViewController.swift
@@ -5,11 +5,12 @@
 //  Created by Javier Etxarri on 11/8/23.
 //
 
-import SwiftUI
+import UIKit
 import EngagementSDK
 
 class WidgetViewController: UIViewController {
-    
+    private let  interactiveTimelineWidgetViewDelegate = InteractiveTimelineWidgetViewDelegate()
+
     var engagementSDK: EngagementSDK!
     var horizontalPadding: CGFloat!
     var error: Error?
@@ -17,26 +18,47 @@ class WidgetViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        engagementSDK.getWidget(id: "99e20b30-3b94-47d9-8e29-bf6b6205eda2", kind: .textPoll) { result in
+        engagementSDK.getWidgetModel(id: "7acec930-d9de-46a2-b9a8-7bd6b68865eb", kind: .textPoll) { result in
             switch result {
                 case .success(let widget):
-                    widget.didMove(toParent: self)
-                    widget.view.frame = CGRect(x: self.horizontalPadding,
-                                               y: widget.view.frame.minY,
-                                               width: widget.view.frame.width - 2 * self.horizontalPadding,
-                                               height: widget.view.frame.height)
-                    self.addChild(widget)
-                    self.view.addSubview(widget.view)
-                                           
-                    // This should not be necessary but it seems that if it does not exists
-                    // the poll does not change to interactive mode
-                    let nextHour = Date().advanced(by: 3600) // One hour in advanced
-                    if widget.widgetModel?.interactiveUntil ?? nextHour > Date() {
-                        widget.moveToNextState()
-                    }
-                   
+                    
+                    self.handleResult(widgetModel: widget)
+                    
                 case .failure(let error):
                     self.error = error
+            }
+        }
+    }
+    
+    func presentWidget(widget: Widget){
+        DispatchQueue.main.async { [weak self] in
+            if let self = self {
+//                widget.theme = self.customTheme
+                widget.view.frame = CGRect(x: self.horizontalPadding,
+                                           y: widget.view.frame.minY,
+                                           width: widget.view.frame.width - 2 * self.horizontalPadding,
+                                           height: widget.view.frame.height)
+                self.addChild(widget)
+                self.view.addSubview(widget.view)
+                widget.delegate = self.interactiveTimelineWidgetViewDelegate
+                widget.moveToNextState()
+            }
+        }
+    }
+    
+    func handleResult(widgetModel: WidgetModel) {
+        if case let .poll(pollModel) = widgetModel {
+            pollModel.loadInteractionHistory { result in
+                switch result {
+                    case .success(_):
+                        let widget = DefaultWidgetFactory.makeWidget(from: widgetModel)
+                        if let widget {
+                            self.presentWidget(widget: widget)
+                        }
+                        
+                    case .failure(let error):
+                        self.error = error
+                }
             }
         }
     }
