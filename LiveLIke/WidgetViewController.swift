aa4f24492f256843d4271436a620e8a9eb1c9ae0
Javier Etxarri <javier.etxarri@openbravo.com>
2023-10-20 15:11:04 +0200

    Update the code to the last production version

--- LiveLIke/WidgetViewController.swift
+++ LiveLIke/WidgetViewController.swift
@@ -17,49 +17,53 @@ class WidgetViewController: UIViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
-        engagementSDK.getWidgetModel(id: "99e20b30-3b94-47d9-8e29-bf6b6205eda2", kind: .textPoll) { result in
-            switch result {
-                case .success(let widget):
-                    
-                    self.handleResult(widgetModel: widget)
-                    
-                case .failure(let error):
-                    self.error = error
-            }
-        }
+        loadWidget()
     }
     
-    func presentWidget(widget: Widget){
-        DispatchQueue.main.async { [weak self] in
-            if let self = self {
-//                widget.theme = self.customTheme
-                widget.view.frame = CGRect(x: self.horizontalPadding,
-                                           y: widget.view.frame.minY,
-                                           width: widget.view.frame.width - 2 * self.horizontalPadding,
-                                           height: widget.view.frame.height)
-                self.addChild(widget)
-                self.view.addSubview(widget.view)
-                widget.delegate = self.interactiveTimelineWidgetViewDelegate
-                widget.moveToNextState()
-            }
-        }
-    }
-    
-    func handleResult(widgetModel: WidgetModel) {
-        if case let .poll(pollModel) = widgetModel {
-            pollModel.loadInteractionHistory { result in
-                switch result {
-                    case .success(_):
-                        let widget = DefaultWidgetFactory.makeWidget(from: widgetModel)
-                        if let widget {
-                            self.presentWidget(widget: widget)
-                        }
-                        
-                    case .failure(let error):
-                        self.error = error
+    // MARK: - Init
+    private func loadWidget() {
+       engagementSDK.getWidgetModel(id: "99e20b30-3b94-47d9-8e29-bf6b6205eda2", kind: .textPoll) { [weak self] result in
+          
+          switch result {
+             case .success(let widgetModel):
+                if case let .poll(pollModel) = widgetModel {
+                   self?.handlePollModel(pollModel, from: widgetModel)
                 }
-            }
-        }
+             case .failure(let error):
+                self?.error = error
+          }
+       }
     }
+    
+    
+    // MARK: - Private
+    private func handlePollModel(_ pollModel: PollWidgetModel, from widgetModel: WidgetModel) {
+       pollModel.loadInteractionHistory { [weak self] result in
+          switch result {
+             case .success:
+                if let widget = self?.makeWidget(widgetModel: widgetModel) {
+                   DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+                      self?.presentWidget(widget)
+                   }
+                }
+             case .failure(let error):
+                self?.error = error
+          }
+       }
+    }
+    
+    private func makeWidget(widgetModel: WidgetModel) -> Widget? {
+       DefaultWidgetFactory.makeWidget(from: widgetModel)
+    }
+     
+     private func presentWidget(_ widget: Widget){
+         widget.view.frame = CGRect(x: horizontalPadding,
+                                   y: widget.view.frame.minY,
+                                   width: widget.view.frame.width - 2 * horizontalPadding,
+                                   height: widget.view.frame.height)
+        addChild(widget)
+        view.addSubview(widget.view)
+        widget.delegate = interactiveTimelineWidgetViewDelegate
+        widget.moveToNextState()
+     }
 }
